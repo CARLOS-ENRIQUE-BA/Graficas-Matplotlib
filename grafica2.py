87322e0beffedeb7f18a525102ce66c94ec1224f
CARLOS-ENRIQUE-BA <carlos_enbagui@hotmail.com>
2024-05-05 03:27:49 -0600

    Added centering of numbers on the x and y axes in graphs, rename a file and add graphs2

--- grafica2.py
+++ grafica2.py
@@ -6,6 +6,7 @@ def f(x):
 
 def f_up(x):
     return x**2 + 2
+
 def f_down(x):
     return x**2 - 2
 
@@ -22,9 +23,12 @@ plt.plot(x, y_up, color='r', label='$f(x) = x^2 + 2$')
 plt.axhline(y=0, color='black', linestyle='-', linewidth=1)
 plt.axvline(x=0, color='black', linestyle='-', linewidth=1) 
 plt.xlabel('x')
-plt.ylabel('y')
 plt.title('Gráfica de $f(x) = x^2 + 2$')
 plt.legend()
+ax = plt.gca()
+ax.spines['left'].set_position('center')
+ax.spines['bottom'].set_position(('data', 0)) 
+plt.xticks([-3, -2, -1, 1, 2, 3])
 plt.grid(True)
 
 plt.subplot(1, 3, 2)
@@ -32,9 +36,12 @@ plt.plot(x, y, color='g', label='$f(x) = x^2$')
 plt.axhline(y=0, color='black', linestyle='-', linewidth=1)
 plt.axvline(x=0, color='black', linestyle='-', linewidth=1) 
 plt.xlabel('x')
-plt.ylabel('y')
 plt.title('Gráfica de $f(x) = x^2$')
 plt.legend()
+ax = plt.gca()
+ax.spines['left'].set_position('center')
+ax.spines['bottom'].set_position(('data', 0)) 
+plt.xticks([-3, -2, -1, 1, 2, 3]) 
 plt.grid(True)
 
 plt.subplot(1, 3, 3)
@@ -42,9 +49,12 @@ plt.plot(x, y_down, color='b', label='$f(x) = x^2 - 2$')
 plt.axhline(y=0, color='black', linestyle='-', linewidth=1)
 plt.axvline(x=0, color='black', linestyle='-', linewidth=1) 
 plt.xlabel('x')
-plt.ylabel('y')
 plt.title('Gráfica de $f(x) = x^2 - 2$')
 plt.legend()
+ax = plt.gca()
+ax.spines['left'].set_position('center')
+ax.spines['bottom'].set_position(('data', 0)) 
+plt.xticks([-3, -2, -1, 1, 2, 3])  # Establecer marcas del eje x excluyendo 0
 plt.grid(True)
 
 plt.tight_layout()
