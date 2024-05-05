f7c8b74c01c2d63db9c7ea5280497613bf74e245
CARLOS-ENRIQUE-BA <carlos_enbagui@hotmail.com>
2024-05-05 17:22:14 -0600

    Add a 2x cos function in the graphic7.py file and also modify the window so that 3 graphics will enter

--- grafica7.py
+++ grafica7.py
@@ -7,13 +7,17 @@ def f_half(x):
 def f(x):
     return np.cos(x)
 
+def f_double(x):
+    return np.cos(2 * x)
+
 x = np.linspace(-10, 10, 400)
 y_half = f_half(x)
 y = f(x)
+y_double = f_double(x)
 
-plt.figure(figsize=(12, 4))
+plt.figure(figsize=(15, 5))
 
-plt.subplot(1, 2, 1)
+plt.subplot(1, 3, 1)
 plt.plot(x, y_half, label='$f(x) = \cos\\left(\\frac{1}{2}x\\right)$', color='b')
 plt.title('Gráfica de $f(x) = \cos\\left(\\frac{1}{2}x\\right)$')
 plt.axhline(y=0, color='black', linestyle='-', linewidth=1)
@@ -25,9 +29,21 @@ plt.gca().spines['top'].set_color('none')
 plt.grid(True)
 plt.legend()
 
-plt.subplot(1, 2, 2)
+plt.subplot(1, 3, 2)
 plt.plot(x, y, label='$f(x) = \cos(x)$', color='r')
 plt.title('Gráfica de $f(x) = \cos(x)$')
+plt.axhline(y=0, color='black', linestyle='-', linewidth=1)
+plt.axvline(x=0, color='black', linestyle='-', linewidth=1)
+plt.gca().spines['left'].set_position(('data', 0))
+plt.gca().spines['right'].set_color('none')
+plt.gca().spines['bottom'].set_position(('data', 0))
+plt.gca().spines['top'].set_color('none')
+plt.grid(True)
+plt.legend()
+
+plt.subplot(1, 3, 3)
+plt.plot(x, y_double, label='$f(x) = \cos(2x)$', color='g')
+plt.title('Gráfica de $f(x) = \cos(2x)$')
 plt.axhline(y=0, color='black', linestyle='-', linewidth=1)
 plt.axvline(x=0, color='black', linestyle='-', linewidth=1)
 plt.gca().spines['left'].set_position(('data', 0))
