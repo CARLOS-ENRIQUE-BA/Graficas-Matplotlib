20c33f184c83f8637a7bdd4aca9a81be152fd908
CARLOS-ENRIQUE-BA <carlos_enbagui@hotmail.com>
2024-05-05 14:25:08 -0600

    Add a new function to the file graphics5.py

--- grafica5.py
+++ grafica5.py
@@ -1,17 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def f(x):
+def f_sin(x):
     return np.sin(x)
-x = np.linspace(-2*np.pi, 2*np.pi, 400)
-y = f(x)
 
-plt.plot(x, y, label='$f(x) = \sin(x)$', color='b')
+def f_abs_sin(x):
+    return np.abs(np.sin(x))
+
+x = np.linspace(-2 * np.pi, 2 * np.pi, 400)
+y_sin = f_sin(x)
+y_abs_sin = f_abs_sin(x)
+
+plt.figure(figsize=(12, 4))
+plt.subplot(1, 2, 1)
+plt.plot(x, y_sin, label='$f(x) = \sin(x)$', color='b')
 plt.title('Gráfica de $f(x) = \sin(x)$')
 plt.legend()
 plt.axhline(y=0, color='black', linestyle='-', linewidth=1)
-plt.axvline(x=0, color='black', linestyle='-', linewidth=1) 
-plt.gca().spines['left'].set_position('center')
-plt.gca().spines['bottom'].set_position('center')
+plt.axvline(x=0, color='black', linestyle='-', linewidth=1)
+plt.gca().spines['left'].set_position(('data', 0))
+plt.gca().spines['right'].set_color('none')
+plt.gca().spines['bottom'].set_position(('data', 0))
+plt.gca().spines['top'].set_color('none')
 plt.grid(True)
+
+plt.subplot(1, 2, 2)
+plt.plot(x, y_abs_sin, label='$f(x) = | \sin(x) |$', color='r')
+plt.title('Gráfica de $f(x) = | \sin(x) |$')
+plt.legend()
+plt.axhline(y=0, color='black', linestyle='-', linewidth=1)
+plt.axvline(x=0, color='black', linestyle='-', linewidth=1)
+plt.gca().spines['left'].set_position(('data', 0))
+plt.gca().spines['right'].set_color('none')
+plt.gca().spines['bottom'].set_position(('data', 0))
+plt.gca().spines['top'].set_color('none')
+plt.grid(True)
+
+plt.tight_layout()
 plt.show()
