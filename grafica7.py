8d09d380f18f742a4bdf88222c1d2da085fe7ae7
CARLOS-ENRIQUE-BA <carlos_enbagui@hotmail.com>
2024-05-05 17:13:34 -0600

    Add Principle of expansion or contraction with respect to the X axis graficas7.py

--- grafica7.py
+++ grafica7.py
@@ -1,15 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def f(x):
+def f_half(x):
     return np.cos(0.5 * x)
 
+def f(x):
+    return np.cos(x)
+
 x = np.linspace(-10, 10, 400)
+y_half = f_half(x)
 y = f(x)
 
-plt.figure(figsize=(10, 5)) 
+plt.figure(figsize=(12, 4))
 
-plt.plot(x, y, label='$f(x) = \cos\\left(\\frac{1}{2}x\\right)$', color='b')
+plt.subplot(1, 2, 1)
+plt.plot(x, y_half, label='$f(x) = \cos\\left(\\frac{1}{2}x\\right)$', color='b')
 plt.title('Gráfica de $f(x) = \cos\\left(\\frac{1}{2}x\\right)$')
 plt.axhline(y=0, color='black', linestyle='-', linewidth=1)
 plt.axvline(x=0, color='black', linestyle='-', linewidth=1)
@@ -19,4 +24,18 @@ plt.gca().spines['bottom'].set_position(('data', 0))
 plt.gca().spines['top'].set_color('none')
 plt.grid(True)
 plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(x, y, label='$f(x) = \cos(x)$', color='r')
+plt.title('Gráfica de $f(x) = \cos(x)$')
+plt.axhline(y=0, color='black', linestyle='-', linewidth=1)
+plt.axvline(x=0, color='black', linestyle='-', linewidth=1)
+plt.gca().spines['left'].set_position(('data', 0))
+plt.gca().spines['right'].set_color('none')
+plt.gca().spines['bottom'].set_position(('data', 0))
+plt.gca().spines['top'].set_color('none')
+plt.grid(True)
+plt.legend()
+
+plt.tight_layout()
 plt.show()
